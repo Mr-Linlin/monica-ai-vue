dbda6039a63f99c41a3fbc1839c534a2c19ad4db
‘linzhentao’ <1908878835@qq.com>
2023-07-18 15:34:00 +0800

    #1

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -2,7 +2,13 @@
   <div class="layout relative">
     <div class="main-bg"></div>
     <div class="absolute w-full h-full top-0 flex justify-center items-center">
-      <div class="fiona-main">内容</div>
+      <div class="fiona-main flex flex-col">
+        <header>头部内容</header>
+        <div class="content flex-1 flex flex-col">
+          <div class="chat-box flex-1">内容</div>
+          <div class="chat-input"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -10,6 +16,7 @@
 .layout {
   width: 100%;
   height: 100vh;
+  font-size: 0.85rem;
   .main-bg {
     width: 100%;
     height: 100%;
@@ -27,6 +34,31 @@
     border-radius: 12px;
     background-color: #fff;
     height: 80%;
+    header {
+      display: flex;
+      align-items: center;
+      height: 50px;
+      min-height: 50px;
+      max-height: 50px;
+      border-bottom: 1px solid #f2f2f2;
+      padding: 0 8px 0 16px;
+      user-select: none;
+    }
+    .content {
+      // background-color: #f1f5f9;
+      border-bottom-left-radius: 12px;
+      border-bottom-right-radius: 12px;
+      .chat-input {
+        width: 100%;
+        padding: 0 16px;
+        z-index: 501;
+        height: 146px;
+        background: #f4f6f8;
+        border-top: 1px solid rgba(145, 158, 171, 0.05);
+        border-bottom-left-radius: 12px;
+        border-bottom-right-radius: 12px;
+      }
+    }
   }
 }
 </style>
